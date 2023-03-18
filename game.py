877e719c98f82d19fc43b06039c183c32669bd59
vibhatsu08 <mistryvedant17@gmail.com>
2023-03-18 14:50:42 +0530

    Added the player sprite class to the game.py file.

--- game.py
+++ game.py
@@ -17,7 +17,28 @@ pygame.init()
 # setting up the screen.
 screen_width = 800
 screen_height = 600
+
+
+# setting up the class for the player sprite.
+# class Player(pygame.sprite.Sprite):
+#     def __init__(self):
+#         super(Player, self).__init__()
+#         self.surf = pygame.Surface((75, 25))
+#         self.surf.fill((255, 0, 255))
+#         self.rect = self.surf.get_rect()
+#
+
+class Player (pygame.sprite.Sprite) :
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 0, 255))
+        self.rect = self.surf.get_rect()
+
+
 screen = pygame.display.set_mode((screen_width, screen_height))
+
+player = Player()
 
 # setting up the key event handlers with the game loop.
 running = True
@@ -32,18 +53,20 @@ while running:
     screen.fill((255, 255, 255))
 
     # now creating a Surface
-    surface = pygame.Surface((50, 50))
-    surface.fill((0, 0, 0))
+    surf = pygame.Surface((50, 50))
+    surf.fill((0, 0, 0))
 
     # creating its underlying rectangle
-    rectangle = surface.get_rect()
+    rectangle = surf.get_rect()
 
     # finding the exact center of the surface to align it both horizontally and vertically in the center.
     surface_center = (
-        (screen_width - surface.get_width())/2,
-        (screen_height - surface.get_height())/2
+        (screen_width - surf.get_width()) / 2,
+        (screen_height - surf.get_height()) / 2
     )
 
     # copying the contents from the surface to the screen.
-    screen.blit(surface, surface_center)
+
+    screen.blit(surf, surface_center)
+    screen.blit(player.surf, (screen_width/2, screen_height/2))
     pygame.display.flip()
